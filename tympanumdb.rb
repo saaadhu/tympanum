17da96b8c1b64e08dd5d7797b7f8a92cc812f13e
Senthil Kumar Selvaraj <ironhide@BrainFart.(none)>
2011-09-01 00:39:04 +0530

    Use MongoHQ for the DB

--- tympanumdb.rb
+++ tympanumdb.rb
@@ -2,12 +2,12 @@ require 'rubygems'
 require 'mongo'
 
 class TympanumDB
-	#@db = Mongo::Connection.new("staff.mongohq.com", 10008).db("tympanum")
-	@db = Mongo::Connection.new.db("tympanum")
+	@db = Mongo::Connection.new("staff.mongohq.com", 10008).db("tympanum")
+	#@db = Mongo::Connection.new.db("tympanum")
 	@auth = false
 
 	def TympanumDB.db
-		#@auth = @db.authenticate('saaadhu', 'genius') unless @auth
+		@auth = @db.authenticate('saaadhu', 'genius') unless @auth
 		@db
 	end
 
