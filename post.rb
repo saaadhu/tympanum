f3ba36275f7f65d8077653d814bef3687f8e6030
Senthil Kumar Selvaraj <senthil.thecoder@gmail.com>
2011-09-01 21:38:14 +0530

    Strip common extensions from index

--- post.rb
+++ post.rb
@@ -40,7 +40,7 @@ class Post
 	def find_keywords
 		url_parts = @url.split(/[\/\. -]/)
 		url_parts.shift(1) #Exclude http
-		url_parts.reject! { |part| /com|net|www|org|biz|in|info|^\d+$/ === part }
+		url_parts.reject! { |part| /com|net|www|org|biz|aspx|asp|php|html|in|info|^\d+$/ === part }
 
 		title_parts = @title.split /[ -]/
 		title_parts.reject! { |part| /^\d+$|in|or|and/ === part }
