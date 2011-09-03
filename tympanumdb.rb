4fa760a7101ba062c1f5bdd865fd8c0ae6fa25a1
Senthil Kumar Selvaraj <senthil.thecoder@gmail.com>
2011-09-03 23:47:00 +0530

    Restored DB url and auth information

--- tympanumdb.rb
+++ tympanumdb.rb
@@ -4,9 +4,9 @@ require 'mongo'
 class TympanumDB
 
 	def TympanumDB.db
-		#@@db ||= Mongo::Connection.new("staff.mongohq.com", 10008).db("tympanum")
-		@@db = Mongo::Connection.new.db("tympanum")
-		#@auth || = @db.authenticate('saaadhu', 'genius') unless @auth
+		@@db ||= Mongo::Connection.new("staff.mongohq.com", 10008).db("tympanum")
+		#@@db = Mongo::Connection.new.db("tympanum")
+		@@auth || = @@db.authenticate('saaadhu', 'genius') unless @@auth
 		@@db
 	end
 
